db6a82c0f759cfb3675821bcf741d8bc50a89939
anton <jaggervid1@gmail.com>
2025-12-07 23:25:08 +0300

    tax

--- motion-canvas/src/scenes/dryKnowledgeScene.tsx
+++ motion-canvas/src/scenes/dryKnowledgeScene.tsx
@@ -1,5 +1,5 @@
 import {makeScene2D, Node, Txt, Circle} from '@motion-canvas/2d';
-import {all, chain, createRef, easeInOutCubic, easeOutBack, easeOutCubic, waitFor} from '@motion-canvas/core';
+import {all, createRef, easeInOutCubic, easeOutCubic, waitFor} from '@motion-canvas/core';
 import {ExplainorTheme} from '../core/theme';
 
 const COLORS = ExplainorTheme.colors;
@@ -9,7 +9,7 @@ export default makeScene2D(function* (view) {
     view.fill(COLORS.background);
 
     const quoteContainer = createRef<Node>();
-    const line1 = createRef<Txt>();
+    const knowledgeWord = createRef<Txt>();
     const line2 = createRef<Txt>();
     const line3 = createRef<Txt>();
     const attribution = createRef<Txt>();
@@ -18,14 +18,13 @@ export default makeScene2D(function* (view) {
     view.add(
         <Node ref={quoteContainer} opacity={0} y={-20}>
             <Txt
-                ref={line1}
                 fontFamily={FONTS.primary}
                 fontSize={36}
                 fill={COLORS.text.primary}
                 textAlign="center"
                 y={-50}
             >
-                Every piece of <Txt fill={COLORS.accent.red} fontWeight={600}>knowledge</Txt> must have
+                Every piece of <Txt ref={knowledgeWord} fill={COLORS.accent.red} fontWeight={600}>knowledge</Txt> must have
             </Txt>
             <Txt
                 ref={line2}
@@ -76,6 +75,9 @@ export default makeScene2D(function* (view) {
     );
 
     yield* waitFor(3);
+
+    const wordPos = knowledgeWord().absolutePosition();
+    knowledgeCircle().absolutePosition(wordPos);
 
     yield* all(
         quoteContainer().opacity(0, 0.5, easeOutCubic),
