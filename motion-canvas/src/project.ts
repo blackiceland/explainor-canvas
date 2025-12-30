4cff5110b7416f26fbe0e13eff839ddf2dd05518
anton <jaggervid1@gmail.com>
2025-12-30 18:52:49 +0300

    title

--- motion-canvas/src/project.ts
+++ motion-canvas/src/project.ts
@@ -1,8 +1,9 @@
 import {makeProject} from '@motion-canvas/core';
+import chapter1IntroScene from './scenes/chapter1IntroScene?scene';
 import dryKnowledgeScene from './scenes/dryKnowledgeScene?scene';
 import dryFiltersScene from './scenes/dryFiltersScene?scene';
 import dryConditionsScene from './scenes/dryConditionsScene?scene';
 
 export default makeProject({
-    scenes: [dryFiltersScene, dryConditionsScene, dryKnowledgeScene]
+    scenes: [chapter1IntroScene, dryFiltersScene, dryConditionsScene, dryKnowledgeScene]
 });
