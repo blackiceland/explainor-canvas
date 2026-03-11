dc279080070140c498f4f70cf3ba127635fdfead
blackiceland <jaggervid1@gmail.com>
2026-03-11 23:03:46 +0300

    2

--- motion-canvas/src/project.ts
+++ motion-canvas/src/project.ts
@@ -73,7 +73,7 @@ export default makeProject({
         jackIntroSceneEn,
         codeWithActionsSceneRu,
         codeWithActionsSceneRuTilt,
-        guitarHeroPassThroughSceneRu,
+        // guitarHeroPassThroughSceneRu,
         contextObjectSceneRu,
         contextRefactorSceneRu,
         globalStateSingletonSceneRu,
